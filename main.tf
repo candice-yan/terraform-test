c0ff66f74743e41f25572d4e617e9106467b6cf7
Kun Yan <kyan@abinitio.com>
2025-08-08 10:57:39 +0800

    2nd vesrsion

--- main.tf
+++ main.tf
@@ -1,12 +1,189 @@
 provider "aws" {
   region = "ap-southeast-1"
+  profile = "personal"
 }
 
-resource "aws_instance" "kyan-ami" {
-  ami           = "ami-0e7f9c9fced6cfb10"
-  instance_type = "t2.micro"
+data "aws_vpc" "default" {
+  default = true
+}
 
-  tags = {
-    Name = "my-terraform-1"
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
+output "public_ip" {
+  value       = aws_lb.kyan_ami.dns_name
+  description = "The domain name of the load balancer"
+}
+
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2_role_for_kyan_ami"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_attach_policy" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
+}
+
+
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "ec2_instance_profile_for_kyan_ami"
+  role = aws_iam_role.ec2_role.name
+}
+
+resource "aws_autoscaling_group" "kyan-ami" {
+  launch_template {
+    id      = aws_launch_template.kyan-ami.id
+    version = "$Latest"
+  }
+  vpc_zone_identifier = data.aws_subnets.default.ids
+
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
+
+  min_size = 1
+  max_size = 3
+  desired_capacity = 1
+}
+
+resource "aws_lb" "kyan_ami" {
+  name = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets = data.aws_subnets.default.ids
+  security_groups = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.kyan_ami.arn
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {  
+  listener_arn = aws_lb_listener.http.arn
+  priority = 100
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+  action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name = "terraform-asg-example"
+  port = var.server_port
+  protocol = "HTTP"
+  vpc_id = data.aws_vpc.default.id
+
+  health_check {
+    path = "/"
+    protocol = "HTTP"
+    matcher = "200"
+    interval = 15
+    timeout = 3
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+  }
+  }
+
+
+
+resource "aws_launch_template" "kyan-ami" {
+  lifecycle {
+    create_before_destroy = true
+  }
+  name_prefix   = "kyan-ami-"
+  image_id      = "ami-0062e0576ebcaa2a9"
+  instance_type = "t2.micro"
+  iam_instance_profile {
+    name = aws_iam_instance_profile.ec2_instance_profile.name
+  }
+  user_data = base64encode(<<-EOF
+#!/bin/bash
+yum install -y python3 || apt-get update && apt-get install -y python3
+echo "Hello, World!" > index.html
+nohup python3 -m http.server ${var.server_port} &
+EOF
+  )
+  network_interfaces {
+    device_index = 0
+    associate_public_ip_address = false
+    security_groups = [aws_security_group.instance.id]
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name = "terraform-example_alb"
+
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+
+resource "aws_security_group" "instance" {
+  name = "my-terraform-1-instance"
+
+  ingress {
+    from_port = var.server_port
+    to_port   = var.server_port
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
